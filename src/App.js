aab8c89069479b6e93c927957ea6aa05c333596c
MIREHMAN <Inammalik73@gmail.com>
2024-10-27 16:16:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/General/Footer";
-import NavBar from "./components/General/NavBar";
 import Homepage from "./pages/Homepage";
 import Products from "./pages/Products";
 import Stores from "./pages/Stores";
 import About from "./pages/About";
-import AdminDashboard from "./pages/Admin Dashboard/AdminDashboard";
 import LoginForm from "./components/General/LoginForm";
 
 function App() {
@@ -21,7 +19,7 @@ function App() {
         <Route path="/stores" element={<Stores />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<LoginForm/>} />
-          <Route path="/dashboard" element={<AdminDashboard/> } />
+        
           
         </Routes>
       </BrowserRouter>
