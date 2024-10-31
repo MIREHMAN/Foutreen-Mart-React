e558339a82f1c19f63e852e533cdd2bf863dcc25
MIREHMAN <Inammalik73@gmail.com>
2024-10-31 19:40:02 +0500

    Update TopCategoriesCard.jsx

--- src/components/TopCategoriesCard.jsx
+++ src/components/TopCategoriesCard.jsx
@@ -14,21 +14,21 @@ const TopCategoriesCard = (backgroundColor='red') => {
         overflow: 'hidden', 
       }}
     >
-      <Box sx={{ m: 2, p:2,flex: 1 }}>
+      <Box sx={{ m: {md:2}, p:2,flex: 1 }}>
         <Stack spacing={1}> 
           <Typography variant="h6" sx={{ fontSize: {xs:'1rem', md:'1.5rem'}, fontWeight: '800', color: 'whitesmoke' }}>
             This is a Heading
           </Typography>
-          <Typography sx={{ fontSize: {xs:'1rem', md:'1.5rem'}, fontWeight: {xs:'400', md:'500'}, color: 'whitesmoke' }}>
+          <Typography sx={{ fontSize: {xs:'0.7rem', md:'1.5rem'}, fontWeight: {xs:'400', md:'500'}, color: 'whitesmoke', lineHeight: {xs:'16px'}}}>
             This is a Paragraph or Description that also includes Promotion
           </Typography>
         </Stack>
-        <Button size='small' variant="contained" color="success" sx={{ mt: 2 }}> 
+        <Button size='small' variant="contained" color="success" sx={{ mt: {md:2, xs:1, fontSize:'0.6rem'} }}> 
           Shop Now
         </Button>
       </Box>
       <Box sx={{ margin: 'auto', marginRight: 2 }}>
-        <img src={cameraImage} height={150} alt="Camera Image" /> 
+        <img src={cameraImage} height={100} alt="Camera Image" /> 
       </Box>
     </Box>
   );
