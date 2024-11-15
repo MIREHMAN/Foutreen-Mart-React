93b379106610c723e49a6e3ad7e6a9a2d19645b8
MIREHMAN <Inammalik73@gmail.com>
2024-11-15 17:36:04 +0500

    Transforming to Next Js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,24 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'React App',
+  description: 'Web site created with Next.js.',
+}
+ 
 export default function RootLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
-    return '...'
+    return (
+      <html lang="en">
+        <head>
+          <title>React App</title>
+          <meta name="description" content="Web site created..." />
+        </head>
+        <body>
+          <div id="root">{children}</div>
+        </body>
+      </html>
+    )
   }
